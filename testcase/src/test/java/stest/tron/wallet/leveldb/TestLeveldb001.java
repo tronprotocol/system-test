319d46a6868b9b7f0b8024879cf4184355afaed6
sophiawang <1975054324@qq.com>
2022-06-21 16:44:06 +0800

    fix

--- testcase/src/test/java/stest/tron/wallet/leveldb/TestLeveldb001.java
+++ testcase/src/test/java/stest/tron/wallet/leveldb/TestLeveldb001.java
@@ -9,15 +9,15 @@ public class TestLeveldb001 extends LeveldbBase {
   public void test01() {
     try {
 
-      System.out.println(getAccountFromDb("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R").toString());
-      System.out.println(getAddressBalance("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R"));
-      System.out.println(getAddressAssetCount("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R", "1000005"));
-      System.out.println(getAssetIssueById("1000005"));
-      String txid = "cdc321bba1856982f5d90b7a2adb0ed19a847003cc670840e8c35d13c2210515";
-      System.out.println(getTransactionById(txid).toString());
-      System.out.println(getTransactionInfoById(txid).toString());
-      System.out.println(getBlockByNumber(311).toString());
-      System.out.println(getNowBlock().toString());
+      //System.out.println(getAccountFromDb("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R").toString());
+      //System.out.println(getAddressBalance("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R"));
+      //System.out.println(getAddressAssetCount("TAsimLx1ZWc6epr5FjitmktrDqhJY2AZ5R", "1000005"));
+      //System.out.println(getAssetIssueById("1000005"));
+      //String txid = "cdc321bba1856982f5d90b7a2adb0ed19a847003cc670840e8c35d13c2210515";
+      //System.out.println(getTransactionById(txid).toString());
+      //System.out.println(getTransactionInfoById(txid).toString());
+      //System.out.println(getBlockByNumber(311).toString());
+      //System.out.println(getNowBlock().toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
