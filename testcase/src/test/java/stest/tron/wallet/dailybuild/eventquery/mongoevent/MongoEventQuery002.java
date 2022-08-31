81717d1586a98b0fff96d2c3542e090fe638be92
danny.guo <danny.guo@tron.network>
2022-08-31 17:44:38 +0800

    add log, fix mongodb event query instable case

--- testcase/src/test/java/stest/tron/wallet/dailybuild/eventquery/mongoevent/MongoEventQuery002.java
+++ testcase/src/test/java/stest/tron/wallet/dailybuild/eventquery/mongoevent/MongoEventQuery002.java
@@ -436,7 +436,7 @@ public class MongoEventQuery002 extends MongoBase {
     }
   }
 
-  @Test(enabled = true, description = "MongoDB Event query for transaction of transfer TRX.")
+  @Test(enabled = true, description = "MongoDB Event query for transaction of transfer TRX.", invocationCount = 5)
   public void test03EventQueryForTransaction() throws InterruptedException {
 
     txId =
@@ -471,8 +471,9 @@ public class MongoEventQuery002 extends MongoBase {
     responseContent = HttpMethed.parseResponseContent(response);
     Long latestSolidifiedBlockNumber =
         responseContent.getJSONObject("block_header").getJSONObject("raw_data").getLong("number");
+    logger.info("mongo latestSolidifiedBlockNumber = {}, block chain solid num = {}", jsonObject.getLong("latestSolidifiedBlockNumber"), latestSolidifiedBlockNumber);
     Assert.assertTrue(
-        jsonObject.getLong("latestSolidifiedBlockNumber") < latestSolidifiedBlockNumber);
+        jsonObject.getLong("latestSolidifiedBlockNumber") <= latestSolidifiedBlockNumber);
 
     Assert.assertTrue(
         (latestSolidifiedBlockNumber - jsonObject.getLong("latestSolidifiedBlockNumber")) < 5);
