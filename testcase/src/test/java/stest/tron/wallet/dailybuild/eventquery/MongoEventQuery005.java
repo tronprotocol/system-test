6ef3e019db23a6cc1a3d434ed192bc4a7b05e76b
xuzibo <511861287@qq.com>
2023-10-07 19:19:40 +0800

    modify check style

--- testcase/src/test/java/stest/tron/wallet/dailybuild/eventquery/MongoEventQuery005.java
+++ testcase/src/test/java/stest/tron/wallet/dailybuild/eventquery/MongoEventQuery005.java
@@ -24,35 +24,15 @@ public class MongoEventQuery005 extends MongoBase {
   private final String foundationKey =
       Configuration.getByPath("testng.conf").getString("foundationAccount.key2");
   private final byte[] foundationAddress = PublicMethed.getFinalAddress(foundationKey);
-
   private ManagedChannel channelFull = null;
   private WalletGrpc.WalletBlockingStub blockingStubFull = null;
   private String fullnode =
       Configuration.getByPath("testng.conf").getStringList("fullnode.ip.list").get(0);
-  private String eventnode =
-      Configuration.getByPath("testng.conf").getStringList("eventnode.ip.list").get(0);
   private Long maxFeeLimit =
       Configuration.getByPath("testng.conf").getLong("defaultParameter.maxFeeLimit");
-
-
   public static String httpFullNode =
       Configuration.getByPath("testng.conf").getStringList("httpnode.ip.list").get(0);
-  public static String httpsolidityNode =
-      Configuration.getByPath("testng.conf").getStringList("httpnode.ip.list").get(3);
 
-
-
-  ECKey ecKey3 = new ECKey(Utils.getRandom());
-
-  List<String> transactionIdList = null;
-
-
-
-  String url = Configuration.getByPath("testng.conf").getString("defaultParameter.assetUrl");
-  Long amount = 1000000L;
-  private String mongoNode =
-      Configuration.getByPath("testng.conf").getStringList("mongonode.ip.list").get(0);
-  private MongoClient mongoClient;
 
 
   /** constructor. */
@@ -63,7 +43,8 @@ public class MongoEventQuery005 extends MongoBase {
   }
 
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in FreezeBalanceV2Contract")
+  @Test(enabled = true, description =
+      "MongoDB Event query for new Field in FreezeBalanceV2Contract")
   public void test01EventQueryForTransactionFreezeBalanceV2() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
@@ -76,14 +57,12 @@ public class MongoEventQuery005 extends MongoBase {
 
     String txid = PublicMethed.freezeBalanceV2AndGetTxId(freezeAccount,
         maxFeeLimit, 0, freezeAccountKey, blockingStubFull);
-
     BasicDBObject query = new BasicDBObject();
     query.put("transactionId", txid);
     FindIterable<org.bson.Document> findIterable =
         mongoDatabase.getCollection("transaction").find(query);
     MongoCursor<org.bson.Document> mongoCursor = findIterable.iterator();
     Document document = null;
-
     int retryTimes = 40;
     while (retryTimes-- > 0) {
       logger.info("retryTimes:" + retryTimes);
@@ -96,7 +75,6 @@ public class MongoEventQuery005 extends MongoBase {
       }
     }
     Assert.assertTrue(retryTimes > 0);
-
     JSONObject jsonObject = JSON.parseObject(document.toJson());
     logger.info(jsonObject.toJSONString());
     Assert.assertEquals(Base58.encode58Check(freezeAccount), jsonObject.getString("fromAddress"));
@@ -105,7 +83,8 @@ public class MongoEventQuery005 extends MongoBase {
   }
 
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in UnFreezeBalanceV2Contract")
+  @Test(enabled = true,
+      description = "MongoDB Event query for new Field in UnFreezeBalanceV2Contract")
   public void test02EventQueryForTransactionUnFreezeBalanceV2() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
@@ -115,21 +94,18 @@ public class MongoEventQuery005 extends MongoBase {
         PublicMethed.sendcoin(
             freezeAccount, freezeAmount, foundationAddress, foundationKey, blockingStubFull));
     PublicMethed.waitProduceNextBlock(blockingStubFull);
-
     PublicMethed.freezeBalanceV2AndGetTxId(freezeAccount,
         maxFeeLimit, 0, freezeAccountKey, blockingStubFull);
     PublicMethed.waitProduceNextBlock(blockingStubFull);
     String txid = PublicMethed.unFreezeBalanceV2AndGetTxId(freezeAccount,
         freezeAccountKey, maxFeeLimit, 0, blockingStubFull);
     PublicMethed.waitProduceNextBlock(blockingStubFull);
-
     BasicDBObject query = new BasicDBObject();
     query.put("transactionId", txid);
     FindIterable<org.bson.Document> findIterable =
         mongoDatabase.getCollection("transaction").find(query);
     MongoCursor<org.bson.Document> mongoCursor = findIterable.iterator();
     Document document = null;
-
     int retryTimes = 40;
     while (retryTimes-- > 0) {
       logger.info("retryTimes:" + retryTimes);
@@ -142,7 +118,6 @@ public class MongoEventQuery005 extends MongoBase {
       }
     }
     Assert.assertTrue(retryTimes > 0);
-
     JSONObject jsonObject = JSON.parseObject(document.toJson());
     logger.info(jsonObject.toJSONString());
     Assert.assertEquals(Base58.encode58Check(freezeAccount), jsonObject.getString("fromAddress"));
@@ -150,7 +125,8 @@ public class MongoEventQuery005 extends MongoBase {
     Assert.assertEquals(maxFeeLimit.longValue(), jsonObject.getLongValue("assetAmount"));
   }
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in CancelAllUnFreezeV2Contract")
+  @Test(enabled = true,
+      description = "MongoDB Event query for new Field in CancelAllUnFreezeV2Contract")
   public void test03EventQueryForTransactionCancelAllUnFreezeV2() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
@@ -190,27 +166,28 @@ public class MongoEventQuery005 extends MongoBase {
       }
     }
     Assert.assertTrue(retryTimes > 0);
-
     JSONObject jsonObject = JSON.parseObject(document.toJson());
     logger.info(jsonObject.toJSONString());
     Assert.assertEquals(Base58.encode58Check(freezeAccount), jsonObject.getString("fromAddress"));
     Assert.assertEquals("trx", jsonObject.getString("assetName"));
-    Assert.assertEquals(maxFeeLimit.longValue(), jsonObject.getJSONObject("extMap").getLongValue("BANDWIDTH"));
+    Assert.assertEquals(maxFeeLimit.longValue(),
+        jsonObject.getJSONObject("extMap").getLongValue("BANDWIDTH"));
   }
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in DelegateResourceContract")
+  @Test(enabled = true,
+      description = "MongoDB Event query for new Field in DelegateResourceContract")
   public void test04EventQueryForTransactionDelegateResource() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
-    String freezeAccountKey = ByteArray.toHexString(ecKey.getPrivKeyBytes());
+    final String freezeAccountKey = ByteArray.toHexString(ecKey.getPrivKeyBytes());
 
     ECKey ecKey2 = new ECKey(Utils.getRandom());
     byte[] receiverAddress = ecKey2.getAddress();
     String receiverKey = ByteArray.toHexString(ecKey2.getPrivKeyBytes());
     PublicMethed.printAddress(receiverKey);
 
-    Long freezeAmount = maxFeeLimit * 20;
-    Long delegateAmount = 10000000L;
+    final Long freezeAmount = maxFeeLimit * 20;
+    final Long delegateAmount = 10000000L;
 
     Assert.assertTrue(
         PublicMethed.sendcoin(
@@ -255,19 +232,20 @@ public class MongoEventQuery005 extends MongoBase {
     Assert.assertEquals(delegateAmount.longValue(), jsonObject.getLongValue("assetAmount"));
   }
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in UnDelegateResourceContract")
+  @Test(enabled = true,
+      description = "MongoDB Event query for new Field in UnDelegateResourceContract")
   public void test05EventQueryForTransactionUnDelegateResource() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
-    String freezeAccountKey = ByteArray.toHexString(ecKey.getPrivKeyBytes());
+    final String freezeAccountKey = ByteArray.toHexString(ecKey.getPrivKeyBytes());
 
     ECKey ecKey2 = new ECKey(Utils.getRandom());
     byte[] receiverAddress = ecKey2.getAddress();
     String receiverKey = ByteArray.toHexString(ecKey2.getPrivKeyBytes());
     PublicMethed.printAddress(receiverKey);
 
-    Long freezeAmount = maxFeeLimit * 20;
-    Long delegateAmount = 10000000L;
+    final Long freezeAmount = maxFeeLimit * 20;
+    final Long delegateAmount = 10000000L;
 
     Assert.assertTrue(
         PublicMethed.sendcoin(
@@ -314,7 +292,8 @@ public class MongoEventQuery005 extends MongoBase {
     Assert.assertEquals(delegateAmount.longValue(), jsonObject.getLongValue("assetAmount"));
   }
 
-  @Test(enabled = true, description = "MongoDB Event query for new Field in WithdrawExpireUnfreezeContract")
+  @Test(enabled = true,
+      description = "MongoDB Event query for new Field in WithdrawExpireUnfreezeContract")
   public void test06EventQueryForTransactionWithdrawExpireUnfreeze() throws InterruptedException {
     ECKey ecKey = new ECKey(Utils.getRandom());
     byte[] freezeAccount = ecKey.getAddress();
@@ -356,14 +335,12 @@ public class MongoEventQuery005 extends MongoBase {
       }
     }
     Assert.assertTrue(retryTimes > 0);
-
     JSONObject jsonObject = JSON.parseObject(document.toJson());
     logger.info(jsonObject.toJSONString());
     Assert.assertEquals(Base58.encode58Check(freezeAccount), jsonObject.getString("fromAddress"));
     Assert.assertEquals("trx", jsonObject.getString("assetName"));
     Assert.assertEquals(maxFeeLimit.longValue(), jsonObject.getLongValue("assetAmount"));
   }
-
 
 
   /** constructor. */
