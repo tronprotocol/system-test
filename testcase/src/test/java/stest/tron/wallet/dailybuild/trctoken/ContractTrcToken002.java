cfd9ae073e185a146ecf20a0cb1a53bccd649af0
albedo xu <511861287@qq.com>
2024-02-01 10:13:20 +0800

    fix unstable case ContractTrcToken002

--- testcase/src/test/java/stest/tron/wallet/dailybuild/trctoken/ContractTrcToken002.java
+++ testcase/src/test/java/stest/tron/wallet/dailybuild/trctoken/ContractTrcToken002.java
@@ -84,7 +84,7 @@ public class ContractTrcToken002 {
 //    Assert.assertTrue(PublicMethed.freezeBalanceForReceiver(fromAddress, 10_000_000L + PublicMethed.randomFreezeAmount.getAndAdd(1),
 //        0, 0, ByteString.copyFrom(dev001Address),
 //        testKey002, blockingStubFull));
-//    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
 
     long start = System.currentTimeMillis() + 2000;
     long end = System.currentTimeMillis() + 1000000000;
