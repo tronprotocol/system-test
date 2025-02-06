ffb75c78254e925f2c2c12a7006728a595989cc6
albedo xu <511861287@qq.com>
2025-02-06 14:57:45 +0800

    fix trident case 2

--- testcase/src/test/java/tron/trident/client/HeaderClientInterceptor.java
+++ testcase/src/test/java/tron/trident/client/HeaderClientInterceptor.java
@@ -12,8 +12,6 @@ public class HeaderClientInterceptor implements ClientInterceptor {
         headers
             .put(Metadata.Key.of("Content-Type", Metadata.ASCII_STRING_MARSHALLER), "application/grpc");
         headers
-            .put(Metadata.Key.of("Host", Metadata.ASCII_STRING_MARSHALLER), "grpc.demo.com");
-        headers
             .put(Metadata.Key.of("x-trace-id", Metadata.ASCII_STRING_MARSHALLER), "testGroupAutoTest");
         headers
             .put(Metadata.Key.of("x-trace-path", Metadata.ASCII_STRING_MARSHALLER), "123 23123");
